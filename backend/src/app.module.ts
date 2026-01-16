2f4cc4cad16c2fd8a03deb08bb342ac43e65f46f
ngobathien <bathienba1@gmail.com>
2026-01-16 16:31:59 +0700

    feat(auth):add mongoose for auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { Connection, Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/booking-hotel', {
+      onConnectionCreate: (connection: Connection) => {
+        connection.on('connected', () => {
+          console.log('Kết nối database thành công');
+        });
+        return connection;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
