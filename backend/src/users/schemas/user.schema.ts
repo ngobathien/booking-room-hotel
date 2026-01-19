450f020e9d788c991c219549679d853a9eeefef1
ngobathien <bathienba1@gmail.com>
2026-01-19 23:30:03 +0700

    feat(users-api): add mongoose user schema

---
+++ backend/src/users/schemas/user.schema.ts
@@ -0,0 +1,45 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+// role
+export enum UserRole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
+
+// status
+export enum UserStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+  BLOCKED = 'BLOCKED',
+}
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  full_name: string;
+
+  @Prop({ required: true, unique: true, lowercase: true })
+  email: string;
+
+  @Prop({ required: true })
+  phone_number: string;
+
+  @Prop({ required: true, select: false })
+  password: string;
+
+  @Prop()
+  avatar: string;
+
+  @Prop({
+    type: String,
+    enum: UserStatus,
+    default: UserStatus.ACTIVE,
+    required: true,
+  })
+  status: UserStatus;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+
+@Module({
+  // import UserSchema từ user.schema.ts
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
