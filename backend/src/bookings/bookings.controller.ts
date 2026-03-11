3a70fd883509690526ba9accec4fc00acd1f9b52
ngobathien <bathienba1@gmail.com>
2026-03-11 14:25:28 +0700

    feat(booking-api): add check availability endpoint

--- backend/src/bookings/bookings.controller.ts
+++ backend/src/bookings/bookings.controller.ts
@@ -14,10 +14,22 @@ import { BookingsService } from './bookings.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { UpdateBookingDto } from './dto/update-booking.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
-
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRole } from 'src/users/schemas/user.schema';
+/* 
+GET    /bookings/check-availability x
+POST   /bookings x
+GET    /bookings x 
+GET    /bookings/my-bookings x
+GET    /bookings/:id 
+PATCH  /bookings/:id/cancel
+PATCH  /bookings/:id/confirm
+PATCH  /bookings/:id/check-in
+PATCH  /bookings/:id/check-out */
 @Controller('bookings')
 export class BookingsController {
   constructor(private readonly bookingsService: BookingsService) {}
+
   // Kiểm tra chi tiết một phòng có sẵn trong khoảng thời gian hay không
   @Get('check-availability')
   async checkRoomAvailability(
@@ -30,25 +42,5 @@ export class BookingsController {
       new Date(checkInDate),
       new Date(checkOutDate),
     );
-  }
-
-  @Get()
-  findAll() {
-    return this.bookingsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.bookingsService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookingDto: UpdateBookingDto) {
-    return this.bookingsService.update(+id, updateBookingDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.bookingsService.remove(+id);
   }
 }
