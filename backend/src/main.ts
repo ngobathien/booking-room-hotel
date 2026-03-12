4c870d7372aed19a94c7df04a77ddb4264f6c4f3
ngobathien <bathienba1@gmail.com>
2026-03-12 13:45:10 +0700

    fix module alias

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,9 @@
+import 'module-alias/register';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
-import 'module-alias/register';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
